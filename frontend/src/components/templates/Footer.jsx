c0340d9ac8db5cfc7374fb568f22c5981ec5c78d
Alifi Cleiton <alificleiton123@gmail.com>
2021-06-07 11:38:06 -0300

    adicionar os arquivos

---
+++ frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import './Footer.css'
+
+
+export default props =>
+        <footer className="footer">
+            <span>
+                Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+                <strong> Alifi Cleiton</strong>
+            </span>
+        </footer>
---
+++ frontend/src/main/Routes.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+
+import Home from '../components/Home/Home'
+import UserCrud from '../components/users/UserCrud'
+
+export default props =>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/users' component={UserCrud} />
+        <Redirect from='*' to='/' />
+      </Switch>
